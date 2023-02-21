e45d48096a1dad6cd8a20e3dccc37fd511eab08b
Назмутдинлв Рустам <russmot@yandex.ru>
2023-02-21 16:29:02 +0300

    Проблема с post.Post.autor и group

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 def index(request):
-    return HttpResponse('Главная страница')
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'title': title
+    }
+    return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Желтая пресса {slug}')
+    title = 'Записи сообщества'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    group = get_object_or_404(Group, slug=slug)
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
