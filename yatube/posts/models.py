d593652980657bf5afd29c470ddeb47ae2ee19ca
Назмутдинлв Рустам <russmot@yandex.ru>
2023-02-22 20:16:23 +0300

    Исправил фаил models и views. ( поменял местами Group c Posts) И ( удалил title = текст в двух функциях)

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -12,16 +21,9 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts')
     group = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
+        Group,
+        on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='post')
+        related_name='posts')
 
-class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField(max_length=100)
-
-    def __str__(self):
-        return self.title
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,21 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 def index(request):
-    title = 'Последние обновления на сайте'
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'posts': posts,
-        'title': title
     }
     return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request, slug):
-    title = 'Записи сообщества'
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     group = get_object_or_404(Group, slug=slug)
     context = {
-        'title': title,
         'group': group,
         'posts': posts,
     }
